2ecbdc8b1f8838291ed7837ca8518fd2aceb45cd
Garrett Fleischer <garrett.fleischer@oit.edu>
2017-03-20 19:42:57 -0700

    Made components immutable. Added routing

--- test/components/Voting_spec.jsx
+++ test/components/Voting_spec.jsx
@@ -6,6 +6,7 @@ import {
   Simulate
 } from 'react-addons-test-utils';
 import {expect} from 'chai'
+import {List} from 'immutable'
 
 import Voting from '../../src/components/Voting';
 
@@ -38,7 +39,7 @@ describe('Voting', () => {
   it('disables buttons when user has voted', () => {
     const component = renderIntoDocument(
       <Voting pair={["Trainspotting", "28 Days Later"]}
-              hasVoted="Trainspotting" />
+      hasVoted="Trainspotting" />
     );
     const buttons = scryRenderedDOMComponentsWithTag(component, 'button');
 
@@ -50,7 +51,7 @@ describe('Voting', () => {
   it('adds label to the voted entry', () => {
     const component = renderIntoDocument(
       <Voting pair={["Trainspotting", "28 Days Later"]}
-              hasVoted="Trainspotting" />
+      hasVoted="Trainspotting" />
     );
     const buttons = scryRenderedDOMComponentsWithTag(component, 'button');
 
@@ -67,6 +68,26 @@ describe('Voting', () => {
     const winner = ReactDom.findDOMNode(component.refs.winner);
     expect(winner).to.be.ok;
     expect(winner.textContent).to.contain('Trainspotting');
+  });
+
+  it('renders as a pure component', () => {
+    const pair = ['Trainspotting', '28 Days Later'];
+    const container = document.createElement('div');
+    let component = ReactDom.render(
+      <Voting pair={pair} />,
+      container
+    );
+
+    let firstButton = scryRenderedDOMComponentsWithTag(component, 'button')[0];
+    expect(firstButton.textContent).to.equal('Trainspotting');
+
+    pair[0] = 'Sunshine';
+    component = ReactDom.render(
+      <Voting pair={pair} />,
+      container
+    );
+    firstButton = scryRenderedDOMComponentsWithTag(component, 'button')[0];
+    expect(firstButton.textContent).to.equal('Trainspotting');
   });
 
 });
